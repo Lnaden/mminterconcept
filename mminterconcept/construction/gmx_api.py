5019bdac34c5658b2a32d993b4c449147e69460c
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-10-23 04:25:19 -0400

    Unit stores output files

--- mminterconcept/construction/gmx_api.py
+++ mminterconcept/construction/gmx_api.py
@@ -5,35 +5,44 @@ DOC: Oct 23, 2019
 GMX (Gromacs) API  for setting up an MD simulation
 '''
 
-from api import Process
-from tools import ImportPDB
-from collections import OrderedDict
+from api import Unit, Engine
+import os
 
-with Process(keep=True) as Pdb2gmx:
+class Gmx(Engine):
 
-	# dict not always ordered (depends on python ver)
-	gmx_args = OrderedDict()
+	def __init__(self, pdbID : str, ff_solute='amber99', ff_solvent='tip3p', topfname='topol.top', 
+			posrename='posre.itp', ofname='system.gro', ext='gro', **args):
 
-	# HACKISH!!
+		super().__init__(pdbID=pdbID, ff_solute=ff_solute, ff_solvent=ff_solvent, topfname=topfname, 
+				ofname=ofname, ext=ext, **args)
 
-	gmx_args['_exec'] = 'gmx'
-	gmx_args['_tool'] = 'pdb2gmx'
-	gmx_args['-f'] = '1LFH.gro'
-	gmx_args['-ff'] = 'amber99'
-	gmx_args['-water'] = 'tip3p'
-	gmx_args['-o'] = 'protein.gro'
-	gmx_args['-p'] = 'topol.top'
-	gmx_args['-i'] = 'posre.itp'
+		self._gmx_args['_exec'] = 'gmx'
+		self._gmx_args['_tool'] = None
+		self._gmx_args['-f'] = os.path.abspath(f'{pdbID}.{ext}')
+		self._gmx_args['-ff'] = ff_solute
+		self._gmx_args['-water'] = ff_solvent
+		self._gmx_args['-o'] = ofname
+		self._gmx_args['-p'] = topfname
+		self._gmx_args['-i'] = posrename
 
-	with ImportPDB(pdbID='1LFH') as SS:
-		SS.save(gmx_args['-f'])
 
-	args = [[key, val] for key, val in gmx_args.items()]
-	args = [single for pair in args for single in pair]
+	def _dict_to_flist(self, **gmx_args) -> list:
+		""" Map dict to flattened list """
+		args = [[key, val] for key, val in gmx_args.items()]
+		args = [single for pair in args for single in pair]
+		return [arg for arg in args if not arg.startswith('_')]
 
-	args = [arg for arg in args if not arg.startswith('_')]
+	def pdb2gmx(self):
+		with Unit(keep=True) as Pdb2gmx:
+			gmx_args = self._gmx_args.copy()
+			# HACKISH!!
+			gmx_args['_tool'] = 'pdb2gmx'
 
-	print(args)
+			args = self._dict_to_flist(**gmx_args)
 
-	Pdb2gmx.run(cmd=args)
-	Pdb2gmx.store(struct=gmx_args['-f'], top=[gmx_args['-p'], gmx_args['-i']])
+			Pdb2gmx.run(cmd=args)
+			Pdb2gmx.store(sdir='..', struct=gmx_args['-o'], top=[gmx_args['-p'], gmx_args['-i']])
+
+if __name__ == '__main__':
+	Test = Gmx(pdbID='1LFH')
+	Test.pdb2gmx()

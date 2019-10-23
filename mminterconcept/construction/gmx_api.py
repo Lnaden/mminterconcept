2d2a01e0d01f114ad76be02d6a183c06c6530c0e
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-10-23 00:49:49 -0400

    gmx_args stored in ordered dict

--- mminterconcept/construction/gmx_api.py
+++ mminterconcept/construction/gmx_api.py
@@ -1,21 +1,39 @@
 '''
 Author: Andrew-AbiMansour
-DOC: Oct 22, 2019
+DOC: Oct 23, 2019
 
-Generic API for MD solvers (e.g. Gromacs)
+GMX (Gromacs) API  for setting up an MD simulation
 '''
 
 from api import Process
 from tools import ImportPDB
+from collections import OrderedDict
 
 with Process(keep=True) as Pdb2gmx:
 
-	speciesfname = '1LFH.gro'
-	species_ff = 'amber99'
-	water_ff = 'tip3p'
+	# dict not always ordered (depends on python ver)
+	gmx_args = OrderedDict()
+
+	# HACKISH!!
+
+	gmx_args['_exec'] = 'gmx'
+	gmx_args['_tool'] = 'pdb2gmx'
+	gmx_args['-f'] = '1LFH.gro'
+	gmx_args['-ff'] = 'amber99'
+	gmx_args['-water'] = 'tip3p'
+	gmx_args['-o'] = 'protein.gro'
+	gmx_args['-p'] = 'topol.top'
+	gmx_args['-i'] = 'posre.itp'
 
 	with ImportPDB(pdbID='1LFH') as SS:
-		SS.save(speciesfname)
+		SS.save(gmx_args['-f'])
 
-	Pdb2gmx.run(cmd=['gmx', 'pdb2gmx', '-f', f'{speciesfname}', '-ff', species_ff, '-water', water_ff, '-o', 'protein.gro', '-p', 'topol.top', '-i', 'posre.itp'])
-	Pdb2gmx.store(struct='protein.gro', top=['topol.top','posre.itp'])
+	args = [[key, val] for key, val in gmx_args.items()]
+	args = [single for pair in args for single in pair]
+
+	args = [arg for arg in args if not arg.startswith('_')]
+
+	print(args)
+
+	Pdb2gmx.run(cmd=args)
+	Pdb2gmx.store(struct=gmx_args['-f'], top=[gmx_args['-p'], gmx_args['-i']])

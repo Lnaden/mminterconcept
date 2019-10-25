92382ceb80ea84820099bcd03cdd4237317d06d4
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-10-25 13:09:04 -0400

    moved gmx_api test to here

--- mminterconcept/construction/test.py
+++ mminterconcept/construction/test.py
@@ -6,19 +6,36 @@ Testing script (eventually will be deleted)
 '''
 
 import sys
-from tools import ImportPDB
-import core
+from gmx_api import Gmx
 
-def run(pdbID):
-	with ImportPDB(pdbID) as Spec:
-        	Species = Spec
+def run(pdbID, mdp='../../../data/1LFH/gmx/mdp/em.mdp'):
 
-	Box = core.Box(bound = ( (0,1),(0,1),(0,1) ))
-	Sys = core.System(Box=Box, Species=Species)
+	Test = Gmx(pdbID=pdbID, box=(9.5, 9, 7))
+
+	# Run pdb2gmx to generate topology
+	Test.genTop()
+
+	# Construct sim box in which the protein is centered 
+	Test.buildBox(center=True)
+
+	# Solvate box with water using the spc216 model
+	Test.solvate(model='spc216')
+
+	# Generate binary (tpr) config file from em.mdp
+	Test.genConfig(mdpfile=mdp, maxwarn=1)
+
+	# Add NaCL (default) ions of salinity 0.1M
+	Test.addIons(conc=0.1, replace='SOL')
+
+	# Update tpr config file
+	Test.genConfig(mdpfile=mdp)
+
+	# Return mdtraj.Trajectory
+	return Test.load()
 
 if __name__ == '__main__':
 	try:
 		pdbID = sys.argv[1]
-		run(pdbID)
+		System = run(pdbID)
 	except Exception:
 		raise

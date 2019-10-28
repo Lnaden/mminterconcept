cf5203a83bf85f4abab77b51a590efdcb448fb4f
Samuel Ellis <sjellis@vt.edu>
2019-10-28 12:54:56 -0400

    Add wrapper components that handle all mdtraj and mdanalysis calculations, respectively.

---
+++ mminterconcept/analysis/mdanalysis_trajectory_analyzer.py
@@ -0,0 +1,32 @@
+from mminterconcept.analysis import mda_rmsd, mda_rog, mda_com, mda_density, mda_rdf
+import MDAnalysis
+import mdtraj
+
+from models import Component
+
+from contextlib import contextmanager
+import os
+from tempfile import TemporaryDirectory
+
+class MDAnalysisTrajectoryComponent(Component):
+    trajectory: mdtraj.Trajectory
+    
+    def __init__(self, trajectory: mdtraj.Trajectory=None):
+        self.trajectory = trajectory
+        
+    def process_input(self, trajectory: mdtraj.Trajectory) -> mdtraj.Trajectory:
+        with TemporaryDirectory() as tempdirname:
+            trajectory.save_pdb(tempdirname+'temp.pdb')
+            self.universe = MDAnalysis.Universe(tempdirname+'temp.pdb')
+        return self.universe
+    
+    def compute(self):
+        pass
+    
+    def run(self, trajectory: mdtraj.Trajectory, cmd: str):
+        cmd_dict = {'rdf': mda_rdf.RDFMDAnalysisComponent, 'rmsd': mda_rmsd.RMSDMDAnalysisComponent, 'rog': mda_rog.ROGMDAnalysisComponent, 'com': mda_com.COMMDAnalysisComponent, 'density': mda_density.DensityMDAnalysisComponent}
+        if cmd not in cmd_dict:
+            raise KeyError('Operation %s is unsupported by this component.' % (cmd))
+        
+        comp = cmd_dict[cmd](trajectory)
+        return comp.run(trajectory)
---
+++ mminterconcept/analysis/mdtraj_trajectory_analyzer.py
@@ -0,0 +1,25 @@
+from mminterconcept.analysis import mdtraj_com, mdtraj_rog, mdtraj_rmsd, mdtraj_rdf, mdtraj_density
+import mdtraj
+
+from models import Component
+
+class MDTrajTrajectoryComponent(Component):
+    trajectory: mdtraj.Trajectory
+    
+    def __init__(self, trajectory: mdtraj.Trajectory=None):
+        self.trajectory = trajectory
+        
+    def process_input(self, trajectory: mdtraj.Trajectory):
+        self.trajectory = trajectory
+        return self.trajectory
+    
+    def compute(self):
+        pass
+    
+    def run(self, trajectory: mdtraj.Trajectory, cmd: str):
+        cmd_dict = {'rdf': mdtraj_rdf.RDFComponent, 'rmsd': mdtraj_rmsd.RMSDComponent, 'rog': mdtraj_rog.ROGComponent, 'com': mdtraj_com.COMComponent, 'density': mdtraj_density.DensityComponent}
+        if cmd not in cmd_dict:
+            raise KeyError('Operation %s is unsupported by this component.' % (cmd))
+        
+        comp = cmd_dict[cmd](trajectory)
+        return comp.run(trajectory)

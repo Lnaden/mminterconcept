0b3a3d74b3debb1bb5922cbfae13923217519e9a
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-10-30 13:34:30 -0400

    added plotting funcs

---
+++ mminterconcept/analysis/plot.py
@@ -0,0 +1,40 @@
+import matplotlib.pylab as plt
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
+
+def plot_com(time_mdt, com_mdt, time_mda, com_mda):
+        plt.plot(time_mdt, com_mdt, '-o')
+        plt.plot(time_mda, com_mda, '-*')
+        plt.xlabel('Time (ps)')
+        plt.ylabel('COM (nm)')
+        plt.grid(linestyle=':')
+        plt.legend(['MDTraj', 'MDAnalysis'])
+        plt.show()
+
+def plot_rmsd(time_mdt, rmsd_mdt, time_mda, rmsd_mda):
+        plt.plot(time_mdt, rmsd_mdt, '-o')
+        plt.plot(time_mda, rmsd_mda, '-*')
+        plt.xlabel('Time (ps)')
+        plt.ylabel('RMSD (nm)')
+        plt.grid(linestyle=':')
+        plt.legend(['MDTraj', 'MDAnalysis'])
+        plt.show()
+
+def plot_rdf(dist_mdt, rdf_mdt, dist_mda, rdf_mda):
+        plt.plot(dist_mdt, rdf_mdt, '--')
+        plt.plot(dist_mda[1:], rdf_mda[1:], '-.')
+        plt.xlabel('Distance (nm)')
+        plt.ylabel('RDF')
+        plt.grid(linestyle=':')
+        plt.legend(['MDTraj', 'MDAnalysis'])
+        plt.show()
+
+def plot_den(time_mdt, den_mdt, time_mda, den_mda):
+        plt.plot(time_mdt, den_mdt, '-o')
+        plt.plot(time_mda, den_mda, '-*')
+        plt.xlabel('Time (ps)')
+        plt.ylabel(r'Density (Kg/$m^3$)')
+        plt.grid(linestyle=':')
+        plt.legend(['MDTraj', 'MDAnalysis'])
+        plt.show()

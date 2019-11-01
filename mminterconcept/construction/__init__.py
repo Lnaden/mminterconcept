9b92c7e0c1832e5f3759f71d7602a14b2b584be9
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-11-01 11:09:04 -0400

    pdbID is a kwarg

--- mminterconcept/construction/__init__.py
+++ mminterconcept/construction/__init__.py
@@ -9,32 +9,32 @@ __all__ = ["protein_constructor",
            "protein_constructor_solvate"
            ]
 
-def protein_constructor(pdbID, mdp, **kwargs) -> Tuple[mdtraj.Trajectory, str]:
+def protein_constructor(mdp, **kwargs) -> Tuple[mdtraj.Trajectory, str]:
     raise NotImplementedError("Generic constructor does not do anything yet")
 
 
-def protein_constructor_vacuum(pdbID, mdp, **kwargs):
-    Eng = initialize(mdp, pdbID=pdbID, **kwargs)
+def protein_constructor_vacuum(mdp, **kwargs):
+    Eng = initialize(mdp, **kwargs)
     Sys, top = Eng.getSystem(), Eng.getTop()
     Eng.clean()
     return Sys, top
 
 
-def protein_constructor_solvate_ion(pdbID, mdp, **kwargs):
+def protein_constructor_solvate_ion( mdp, **kwargs):
     """ Returns mdtraj.Trajectory """
     if "salinity" in kwargs:
         salinity = kwargs["salinity"]
     else:
         salinity = 0.1
-    Eng = initialize(mdp, pdbID=pdbID, **kwargs)
+    Eng = initialize(mdp, **kwargs)
     Eng = solvate(Eng, mdp)
     Eng = ionize(Eng, salinity, mdp)
     Sys, top = Eng.getSystem(), Eng.getTop()
     Eng.clean()
     return Sys, top
 
-def protein_constructor_solvate(pdbID, mdp, **kwargs):
-    Eng = initialize(mdp, pdbID=pdbID, **kwargs)
+def protein_constructor_solvate(mdp, **kwargs):
+    Eng = initialize(mdp, **kwargs)
     Eng = solvate(Eng, mdp)
     Sys, top = Eng.getSystem(), Eng.getTop()
     Eng.clean()
